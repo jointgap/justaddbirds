ff6a157bed7df7065ae7d1459354bbbcffe73561
James Stewart <jointgap@gmail.com>
2015-05-30 02:58:24 +0800

    Initialize

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.justaddbirds.co.uk/admin/');
+define('HTTP_CATALOG', 'http://local.justaddbirds.co.uk/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.justaddbirds.co.uk/admin/');
+define('HTTPS_CATALOG', 'http://local.justaddbirds.co.uk/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/admin/');
+define('DIR_SYSTEM', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/');
+define('DIR_LANGUAGE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/admin/language/');
+define('DIR_TEMPLATE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/admin/view/template/');
+define('DIR_CONFIG', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/config/');
+define('DIR_IMAGE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/image/');
+define('DIR_CACHE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/cache/');
+define('DIR_DOWNLOAD', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/download/');
+define('DIR_UPLOAD', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/upload/');
+define('DIR_LOGS', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/logs/');
+define('DIR_MODIFICATION', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/modification/');
+define('DIR_CATALOG', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'justaddb');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.justaddbirds.co.uk/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.justaddbirds.co.uk/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/catalog/');
+define('DIR_SYSTEM', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/');
+define('DIR_LANGUAGE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/catalog/language/');
+define('DIR_TEMPLATE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/config/');
+define('DIR_IMAGE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/image/');
+define('DIR_CACHE', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/cache/');
+define('DIR_DOWNLOAD', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/download/');
+define('DIR_UPLOAD', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/upload/');
+define('DIR_MODIFICATION', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/modification/');
+define('DIR_LOGS', 'E:/xampp/htdocs/www.justaddbirds.co.uk/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'justaddb');
+define('DB_PREFIX', 'oc_');
